7cacfd413f359917575843ee815f80e540952bfb
Vinicios Jose Gonçalves de Jesus <vinicios.goncalves@compasso.com.br>
2023-10-28 23:02:44 -0300

    com objeto json

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ const app = require ('express')();
 app.listen (3000, () => console.log('Server running'));
 
 app.get('/skills', (req, res) => {
-    res.send('aqui');
+    res.send({
+        javascript: 10,
+        node: 10
+    });
 })
